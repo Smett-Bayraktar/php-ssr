aa04cce99b02e45f4aeb55a334f88820c0d73d7e
oscar <oscarpalma.vpr2@gmail.com>
2023-08-16 13:33:03 -0400

    php-ssr

---
+++ layout/aside.php
@@ -0,0 +1,7 @@
+<aside>
+    <h3>Publicidad</h3>
+    <div class="anuncios">
+        <img src="img/ad1.jpg" width='250px'>
+        <img src="img/ad2.jpg" width='250px'>
+    </div>
+</aside>
---
+++ layout/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Mi Página Web</title>
+</head>
+<body>
+
+    <div class="contenedor">
+
+        <header>
+            <h3 class="logo">OSCAR PALMA</h3>
+
+            <nav>
+                <a href="index.php">Inicio</a>
+                <a href="acerca-de.php">Acerca de</a>
+                <a href="contacto.php">Contacto</a>
+            </nav>
+        </header>
+
+        <div class="contenido">
+
+    
